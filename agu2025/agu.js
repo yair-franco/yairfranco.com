e35c7c8925c6cd3b2d1bfd89592da86991e5ecc6
yair-franco <fran.yair.co@gmail.com>
2025-12-02 18:31:37 -0800

    updated poster and contact info

--- agu2025/agu.js
+++ agu2025/agu.js
@@ -1,13 +1,34 @@
 $(function() {
     $("#showPoster").one("click", showPoster);
+
+    o.onchange = function() {
+        if (isMobileDevice()){
+            if (o.type.startsWith('portrait')){
+                $(".topCont").show();
+                $("#rec").show();
+            } else {
+                $(".topCont").hide();
+                $("#rec").hide();
+            }
+        }
+    }
 })
+
+const o = window.screen.orientation;
+
+function isMobileDevice() {
+    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    return /android|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(userAgent);
+}
 
 function showPoster() {
     const iframe = $("#posterFrame");
     if (!iframe.attr("src")) iframe.attr("src", iframe.data("src"));
     iframe.show();
+    $("#rec").show();
     $("#showPoster")
         .html("Click to toggle poster")
+        .css({"font-size": "16px"})
         .on("click", function(){
             iframe.toggle();
         })
